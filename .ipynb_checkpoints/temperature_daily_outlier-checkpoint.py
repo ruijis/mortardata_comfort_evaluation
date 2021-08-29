19f8b42d3e85ec093e446228aca6960086c96187
Ricky <45080751+ruijis@users.noreply.github.com>
2021-08-29 15:03:51 -0700

    correct daily_outlier percentile threshold calculation

--- .ipynb_checkpoints/temperature_daily_outlier-checkpoint.py
+++ .ipynb_checkpoints/temperature_daily_outlier-checkpoint.py
@@ -1,8 +1,9 @@
 """Import packages for data engineering"""
 import pandas as pd
 import glob
+import matplotlib.pyplot as plt
 
-def temp_daily(a, b, p, f, e):
+def temp_daily(a, b, t, f, e):
     """
     Calculate the percentage of occupied time whose range is outside the threshold.
     The data file type should be CSV.
@@ -15,8 +16,8 @@ def temp_daily(a, b, p, f, e):
         The start time (24-hour clock) of normal office hours during weekdays
     b : int
         The end time (24-hour clock) of normal office hours during weekdays
-    p : float
-        the precentile threshold of daily temperature range, eg. 0.8
+    t : float
+        the precentile threshold of all measure daily temperature range, eg. 0.8
     f : string
         path of the folder that stores inporting CSV files
     e : string
@@ -29,37 +30,44 @@ def temp_daily(a, b, p, f, e):
     """
     path = glob.glob(f"{f}/*.csv")
     res_zone = []
-    res_value = []
     res_threshold = []
+    res_percentage = []
     for i in path:
         df = pd.read_csv(i)
-        time = df.columns[0]
-        temp = df.columns[1]
+        # rename the column name
+        df.columns = ['datetime', 'temp']
         # get the date from the time for groupby function later on
-        df['date'] = pd.to_datetime(df[time]).dt.date
-        df['hour'] = pd.to_datetime(df[time]).dt.hour
-        df['weekdays'] = pd.to_datetime(df[time]).dt.dayofweek
-        # create a new dataframe for the specified office hours and weekdays
+        df['date'] = pd.to_datetime(df['datetime']).dt.date
+        df['hour'] = pd.to_datetime(df['datetime']).dt.hour
+        df['weekdays'] = pd.to_datetime(df['datetime']).dt.dayofweek
+        # calculate the percetile threshold value by all daily tempearture range
+        df_max = df.groupby(['date']).max()
+        df_min = df.groupby(['date']).min()
+        df_max['range'] = df_max['temp'] - df_min['temp']
+        t_value = df_max['range'].quantile(t)
+        # occupied dataframe for the specified office hours and weekdays
         df_occ = df[(df['hour'] >= a) & (df['hour'] < b) &
                     (df['weekdays'] >= 0) & (df['weekdays'] <= 4)]
-        # calculate daily temperature range by max minus min
-        df_max = df_occ.groupby(['date']).max()
-        df_min = df_occ.groupby(['date']).min()
-        df_range = df_max[temp] - df_min[temp]
-        # get rows from the new dataframe that are out of the threshold
-        t = df_range.quantile(p)
-        df_out = df_range[(df_range > t)]
-        # Calculate the percentage of occupied time outside the threshold
-        p = len(df_out) / len(df_max)
+        # calculate occupied daily temperature range by max minus min
+        df_occ_max = df_occ.groupby(['date']).max()
+        df_occ_min = df_occ.groupby(['date']).min()
+        # add a new column to the df_max called range
+        df_occ_max['range'] = df_occ_max['temp'] - df_occ_min['temp']
+        # create a new df containing rows that are out of the threshold
+        df_out = df_occ_max[(df_occ_max['range'] > t_value)]
+        p = len(df_out) / len(df_occ)
+        # plot the weekday frequency
+        plt.figure()
+        df_out['weekdays'].plot.hist()
         # store the zone name and percentage values for later sort
         res_zone.append(i.partition(f)[2])
-        res_value.append(p)
-        res_threshold.append(t)
+        res_threshold.append(t_value)
+        res_percentage.append(p)
         # export the dates that are out of the range
-        df_out.rename("daily range").to_csv(f"{e}/{i.partition(f)[2]}")
+        df_out.to_csv(f"{e}/{i.partition(f)[2]}")
     res = pd.DataFrame({'zone name': res_zone,
-                        'outside percentage': res_value,
-                        f'{p}percentile threshold': res_threshold})
-    return res.sort_values(['outside percentage'], ascending=[False]) 
+                        f'{t}percentile threshold': res_threshold,
+                        'outlier percentage': res_percentage})
+    return res.sort_values(['outlier percentage'], ascending=[False]) 
 
 
